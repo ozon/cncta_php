0ba5f5e723cc7646d540de3465780427218a1570
Harry Gabriel <rootdesign@gmail.com>
2012-11-22 23:25:58 +0100

    add GPL license header

--- cncta.class.php
+++ cncta.class.php
@@ -1,5 +1,23 @@
 <?php
 
+/**
+ * GNU Public License 3.0
+ * Copyright (C) 2012 Harry Gabriel
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 class TiberiumAlliances
 {
     private static $_instance = null;
@@ -101,8 +119,8 @@ class TiberiumAlliances
       $this->_session->key = $result->i;
     }
 
-    public function getData( $url=Null, $endpoint, $data ) {
-
+    public function getData( $url=Null, $endpoint, $data )
+    {
       $ch = curl_init();
       curl_setopt( $ch, CURLOPT_URL, $url . $endpoint );
       curl_setopt( $ch, CURLOPT_CONNECTTIMEOUT, 2 );
